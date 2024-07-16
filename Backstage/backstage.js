05e2aea77c4dea2ca5cee52b5d933fe622253a96
Eugene16342 <WOW23875708@gmail.com>
2024-07-16 21:49:43 +0800

    增加了關聯表的連結，新增了點閱排行

--- Backstage/backstage.js
+++ Backstage/backstage.js
@@ -27,12 +27,18 @@ db.connect((err) => {
   console.log("Connected to database");
 });
 
+//以上都是設定
+
+//首頁
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+//recipe路由  recipe_common為SQL指令，讓recipe表連接style和related
+const recipe_common =
+  "SELECT recipe.*, style.style_name AS style_name, related.related_name AS related_name FROM recipe LEFT JOIN style ON recipe.style = style.style_uid LEFT JOIN related ON recipe.related = related.related_uid";
 app.get("/recipe", (req, res) => {
-  let sql = "SELECT * FROM recipe";
+  let sql = recipe_common;
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.render("recipe", { items: result });
@@ -40,7 +46,23 @@ app.get("/recipe", (req, res) => {
 });
 
 app.get("/recipe/sort/recipe_uid", (req, res) => {
-  let sql = "SELECT * FROM recipe ORDER BY recipe_uid DESC";
+  let sql = recipe_common + " ORDER BY recipe.recipe_uid DESC;";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result });
+  });
+});
+
+app.get("/recipe/sort/click", (req, res) => {
+  let sql = recipe_common + " ORDER BY click DESC";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result });
+  });
+});
+
+app.get("/recipe/sort/click_desc", (req, res) => {
+  let sql = recipe_common + " ORDER BY click";
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.render("recipe", { items: result });
@@ -55,8 +77,11 @@ app.get("/recipe/:id", (req, res) => {
   });
 });
 
+//product路由  product_common和食譜的作用一樣
+const product_common =
+  "SELECT product.*, related.related_name AS related_name FROM product LEFT JOIN related ON product.related = related.related_uid";
 app.get("/product", (req, res) => {
-  let sql = "SELECT * FROM product";
+  let sql = product_common;
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.render("product", { items: result });
@@ -64,7 +89,23 @@ app.get("/product", (req, res) => {
 });
 
 app.get("/product/sort/product_uid", (req, res) => {
-  let sql = "SELECT * FROM product ORDER BY product_uid DESC";
+  let sql = product_common + " ORDER BY product_uid DESC";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("product", { items: result });
+  });
+});
+
+app.get("/product/sort/click", (req, res) => {
+  let sql = product_common + " ORDER BY click DESC";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("product", { items: result });
+  });
+});
+
+app.get("/product/sort/click_desc", (req, res) => {
+  let sql = product_common + " ORDER BY click";
   db.query(sql, (err, result) => {
     if (err) throw err;
     res.render("product", { items: result });
