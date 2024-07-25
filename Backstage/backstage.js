9232ab2b26c529237d378df808b534411df61cc5
Eugene16342 <WOW23875708@gmail.com>
2024-07-25 11:20:25 +0800

    新增商品編輯功能

--- Backstage/backstage.js
+++ Backstage/backstage.js
@@ -274,59 +274,8 @@ app.post("/deleteProducts", function (req, res) {
     }
   });
 });
-/////////////////////recipe路由  recipe_common為SQL指令，讓recipe表連接style和related
-const recipe_common =
-  "SELECT recipe.*, style.style_name AS style_name, related.related_name AS related_name FROM recipe LEFT JOIN style ON recipe.style = style.style_uid LEFT JOIN related ON recipe.related = related.related_uid";
-app.get("/recipe", (req, res) => {
-  let sql = recipe_common;
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.render("recipe", { items: result });
-  });
-});
 
-app.get("/recipe/sort/recipe_uid", (req, res) => {
-  let sql = recipe_common + " ORDER BY recipe.recipe_uid DESC;";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.render("recipe", { items: result });
-  });
-});
-
-app.get("/recipe/sort/click", (req, res) => {
-  let sql = recipe_common + " ORDER BY click DESC";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.render("recipe", { items: result });
-  });
-});
-
-app.get("/recipe/sort/click_desc", (req, res) => {
-  let sql = recipe_common + " ORDER BY click";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    res.render("recipe", { items: result });
-  });
-});
-
-app.get("/recipe/sort", (req, res) => {
-  let style = req.query.style;
-  let sql = recipe_common + " WHERE style = ?";
-  db.query(sql, [style], (err, result) => {
-    if (err) throw err;
-    res.render("recipe", { items: result, style: style });
-  });
-});
-
-app.get("/recipe/:id", (req, res) => {
-  let sql = "SELECT * FROM recipe WHERE recipe_uid = ?";
-  db.query(sql, [req.params.id], (err, result) => {
-    if (err) throw err;
-    res.render("recipe_detail", { items: result[0] });
-  });
-});
-
-////////////////////編輯食譜頁面
+////////////////////////////////////編輯食譜頁面
 app.get("/recipe/edit/:id", (req, res) => {
   let sql1 = "SELECT * FROM recipe WHERE recipe_uid = ?";
   let sql2 = "SELECT * FROM ingredients_for_recipe WHERE recipe_uid = ?";
@@ -353,20 +302,7 @@ app.get("/recipe/edit/:id", (req, res) => {
     });
 });
 
-//////////////////////////////////////////編輯商品頁面
-app.get("/product/edit/:id", (req, res) => {
-  let sql1 = "SELECT * FROM product WHERE product_uid = ?";
-  db.query(sql1, [req.params.id], (err, result) => {
-    if (err) {
-      // 處理錯誤
-      console.log(err);
-    } else {
-      res.render("edit_product", { items: result[0] });
-    }
-  });
-});
-
-////////////////////編輯食譜功能
+/////////////////////////////////////////////////編輯食譜功能
 const uploadForEdit = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
@@ -423,6 +359,110 @@ app.post("/recipe/edit/:id", uploadForEdit.single("file"), (req, res) => {
       });
     }
   );
+});
+
+/////////////////////////////////////////////////編輯商品頁面
+app.get("/product/edit/:id", (req, res) => {
+  let sql1 = "SELECT * FROM product WHERE product_uid = ?";
+  db.query(sql1, [req.params.id], (err, result) => {
+    if (err) {
+      // 處理錯誤
+      console.log(err);
+    } else {
+      res.render("edit_product", { items: result[0] });
+    }
+  });
+});
+//////////////////////////////////////////////////編輯商品功能
+const uploadProudctEdit = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "../img/product");
+    },
+    filename: function (req, file, cb) {
+      cb(null, "product" + req.params.id + path.extname(file.originalname));
+    },
+  }),
+});
+
+app.post("/product/edit/:id", uploadProudctEdit.single("file"), (req, res) => {
+  let uid = req.params.id;
+  let data = req.body;
+  let sql1 =
+    "UPDATE product SET product_title = ?, part_describe = ?, full_describe = ?,size = ?,price = ?, inventory = ?, sales_amount = ?, related = ? WHERE product_uid = ?";
+  db.query(
+    sql1,
+    [
+      data.product_title,
+      data.part_describe,
+      data.full_describe,
+      data.size,
+      data.price,
+      data.inventory,
+      data.sales_amount,
+      data.related,
+      uid,
+    ],
+    function (err, result) {
+      if (err) {
+        console.error(err);
+        res.send("更新失敗，請稍後再試！");
+      } else {
+        res.json({ message: "資料已成功更新！" });
+      }
+    }
+  );
+});
+/////////////////////recipe路由  recipe_common為SQL指令，讓recipe表連接style和related
+const recipe_common =
+  "SELECT recipe.*, style.style_name AS style_name, related.related_name AS related_name FROM recipe LEFT JOIN style ON recipe.style = style.style_uid LEFT JOIN related ON recipe.related = related.related_uid";
+app.get("/recipe", (req, res) => {
+  let sql = recipe_common;
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result });
+  });
+});
+
+app.get("/recipe/sort/recipe_uid", (req, res) => {
+  let sql = recipe_common + " ORDER BY recipe.recipe_uid DESC;";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result });
+  });
+});
+
+app.get("/recipe/sort/click", (req, res) => {
+  let sql = recipe_common + " ORDER BY click DESC";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result });
+  });
+});
+
+app.get("/recipe/sort/click_desc", (req, res) => {
+  let sql = recipe_common + " ORDER BY click";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result });
+  });
+});
+
+app.get("/recipe/sort", (req, res) => {
+  let style = req.query.style;
+  let sql = recipe_common + " WHERE style = ?";
+  db.query(sql, [style], (err, result) => {
+    if (err) throw err;
+    res.render("recipe", { items: result, style: style });
+  });
+});
+
+app.get("/recipe/:id", (req, res) => {
+  let sql = "SELECT * FROM recipe WHERE recipe_uid = ?";
+  db.query(sql, [req.params.id], (err, result) => {
+    if (err) throw err;
+    res.render("recipe_detail", { items: result[0] });
+  });
 });
 
 //////////////////////////////product路由  product_common和食譜的作用一樣
